6fa1f9bef01188b1f7f0f38dd6519c404006f2ec
Bryan Finlayson <bdfinlayson@gmail.com>
2015-02-12 20:28:26 -0600

    first test

---
+++ tests/spec/main.test.js
@@ -0,0 +1,8 @@
+/* jshint mocha: true, expr: true */
+
+describe('test suite', function() {
+  it('should assert true', function() {
+    true.should.be.true;
+    false.should.be.false;
+  });
+});
