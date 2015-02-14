bb55ad8534260c0fd9b7c55f845ed1e3032c24ea
Bryan Finlayson <bdfinlayson@gmail.com>
2015-02-14 16:28:56 -0600

    added color picking functionality in js file

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,18 @@ function hello () {
 function addUp (num) {
   return num + num;
 }
+
+$(document).ready(function () {
+
+  var paint = "white"
+
+  $(".color")white.on("click", function () {
+    paint = $(this).css('background-color');
+          })
+
+          $('.row').on('click', function () {
+
+            $(this).css("background-coloror", paint );
+          });
+
+});
